6c0c3cd8c2e35c962e20e422ad88c8e732916284
samir-ben <samir-69@hotmail.fr>
2018-12-17 21:49:33 +0100

    Initial : component and config
    - Add a navbar component
    - add a list books component
    - add a create component
    - add an edit component
    - add materialize css

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ListBooksComponent } from './list-books/list-books.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListBooksComponent,
+    CreateComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
