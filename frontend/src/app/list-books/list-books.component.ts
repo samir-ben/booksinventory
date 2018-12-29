0e5750b8ee862aa220503be737ebfe95d62c8466
samir-ben <samir-69@hotmail.fr>
2018-12-29 18:08:41 +0100

    feat: angular material table
    - add angular material table
    - add template
    - add method for displaying, filtering
    - add some style for table

--- frontend/src/app/list-books/list-books.component.ts
+++ frontend/src/app/list-books/list-books.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { BookService } from '../book.service';
 import { Book } from '../book.model';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { debounceTime, tap, switchMap} from 'rxjs/operators';
+import { debounceTime, tap, switchMap } from 'rxjs/operators';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+
 
 @Component({
   selector: 'app-list-books',
@@ -12,33 +14,55 @@ import { debounceTime, tap, switchMap} from 'rxjs/operators';
   styleUrls: ['./list-books.component.css']
 })
 export class ListBooksComponent implements OnInit {
-
   books: Book[];
   searchTerm = new FormControl();
   searchTerms$: Observable<string> = this.searchTerm.valueChanges;
   result: Book[] = [];
 
-  constructor(private bookService: BookService, private router: Router) { }
+  constructor(private bookService: BookService, private router: Router) {}
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  MyDataSource: any;
+  bookList: Book[];
+  id: any;
+
+  displayedColumns = [
+    'Titre',
+    'Auteur',
+    'PAHT unitaire',
+    'PVHT unitaire',
+    'PVTTC unitaire',
+    'Coût stock total',
+    'Réduction (%)',
+    'PVTTC après réduction',
+    'Quantité',
+    'Catégorie',
+    'Etat',
+    'Actions'
+  ];
 
   ngOnInit() {
     this.fetchBooks();
     this.searchTerms$
-      .pipe(
-        debounceTime(1000),
-        switchMap(rq => this.bookService.search(rq.toLowerCase())),
-        tap(x => console.log(x))
-      )
-      .subscribe(term => this.result = term);
+      .pipe( debounceTime(1000), switchMap(rq =>
+          this.bookService.search(rq.toLowerCase())
+        ), tap(x => console.log(x)) )
+      .subscribe(term => (this.MyDataSource = term));
   }
 
   fetchBooks() {
-    this.bookService
-      .getBooks()
-      .subscribe((data: Book[]) => {
-        this.books = data;
-        console.log('Data requested ...');
-        console.log(this.books);
-      });
+    this.bookService.getBooks().subscribe((data: Book[]) => {
+      console.log(this.books);
+      this.MyDataSource = new MatTableDataSource();
+      this.MyDataSource.data = data;
+      this.MyDataSource.paginator = this.paginator;
+      this.MyDataSource.sort = this.sort;
+      this.books = this.MyDataSource;
+      this.id = this.MyDataSource._data;
+      console.log('id', this.id);
+    });
   }
 
   editBook(id) {
@@ -61,7 +85,6 @@ export class ListBooksComponent implements OnInit {
   }
 
   getPriceWithDiscount(value1, value2) {
-    return value1 - (value1 * value2 / 100) ;
+    return value1 - (value1 * value2) / 100;
   }
-
 }
